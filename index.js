ef217862a031380a43d18afbde6d2d82a721d52b
Danny Joe Khoury <dannyjoekhoury@gmail.com>
2022-10-19 22:23:46 -0400

    commit tree bfd5f39bc13deb9e7e0c4dd01eda2f49ae6a3851

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+
+const submitData = {
+    name: "Byron",
+    email: "poodle@gmail.com",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(submitData),
+  };
+  
+  fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+
+    .catch(function (error) {
+        alert("Unauthorized  Access");
+        console.log(error.message);
+    });
